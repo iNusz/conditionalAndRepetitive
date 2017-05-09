cf029e011341714a9fb661da2a9380cfaffdbeb5
SeungHo Shin <SeungHoShin@SeungHoui-MacBook-Pro.local>
2017-05-09 22:22:02 +0900

    charexample

---
+++ SwitchCharExample1.java
@@ -0,0 +1,24 @@
+package conditionalAndRepetitive;
+
+public class SwitchCharExample1 {
+
+	public static void main(String[] args) {
+		char grade = 'c';
+		
+		switch(grade) {
+		
+		case 'A':
+		case 'a':
+			System.out.println("우수 회원입니다.");
+				break;
+		case 'B':
+		case 'b':
+			System.out.println("일반 회원입니다.");
+				break;
+		default:
+			System.out.println("손님입니다.");
+		}
+
+	}
+
+}
